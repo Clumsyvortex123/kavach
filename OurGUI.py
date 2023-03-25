ca5d3a4135816b308567a326e66efda34c36b468
Clumsyvortex123 <91614556+Clumsyvortex123@users.noreply.github.com>
2023-03-25 14:10:47 +0530

    Update OurGUI.py

--- OurGUI.py
+++ OurGUI.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 
-# Form implementation generated from reading ui file 'OurGUI.ui'
+# Form implementation generated from reading ui file 'MyDemoUIfile.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
@@ -17,26 +17,42 @@ import os
 from csv import DictWriter
 from getpass import getpass
 from cvzone.PoseModule import PoseDetector
-class Ui_MyInterface(object):
-    def setupUi(self, MyInterface):
-        MyInterface.setObjectName("MyInterface")
-        MyInterface.resize(639, 572)
 
-        self.VideoStreamer = QtWidgets.QPushButton(MyInterface)
-        self.VideoStreamer.setGeometry(QtCore.QRect(70, 280, 111, 25))
-        self.VideoStreamer.setObjectName("VideoStreamer")
-        self.VideoStreamer.clicked.connect(self.myvideo)
-
-        self.DataBase = QtWidgets.QPushButton(MyInterface)
-        self.DataBase.setGeometry(QtCore.QRect(470, 280, 89, 25))
-        self.DataBase.setObjectName("DataBase")
-        self.DataBase.clicked.connect(self.openMyFile)
-
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(1051, 574)
+        Dialog.setStyleSheet("#VideoStream{\n"
+"    background-color:\"lightblue\";\n"
+"}\n"
+"#DataSet{\n"
+"    background-color:\"lightblue\";\n"
+"}\n"
+"#Dialog{\n"
+"    background-color: \"white\";\n"
+"}\n"
+"#QPushButton::cliked{\n"
+"    background-color:\"blue\";\n"
+"}")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(-630, -10, 1721, 1041))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("../../Downloads/mycctv.jpg"))
+        self.label.setObjectName("label")
         
+        self.VideoStream = QtWidgets.QPushButton(Dialog)
+        self.VideoStream.setGeometry(QtCore.QRect(100, 150, 141, 41))
+        self.VideoStream.setObjectName("VideoStream")
+        self.VideoStream.clicked.connect(self.myvideo)
+        
+        self.DataSet = QtWidgets.QPushButton(Dialog)
+        self.DataSet.setGeometry(QtCore.QRect(100, 350, 141, 41))
+        self.DataSet.setObjectName("DataSet")
+        self.DataSet.clicked.connect(self.openMyFile) 
 
-        self.retranslateUi(MyInterface)
-        QtCore.QMetaObject.connectSlotsByName(MyInterface)
-
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+    
     def myvideo(self):
         cmd=["python3","newtest.py"]
         sp.Popen(cmd)
@@ -45,21 +61,20 @@ class Ui_MyInterface(object):
          YourCSV=["python3","csvOpener.py"]
          sp.Popen(YourCSV)
 
-
-    def retranslateUi(self, MyInterface):
+    def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        MyInterface.setWindowTitle(_translate("MyInterface", "Dialog"))
-        self.VideoStreamer.setText(_translate("MyInterface", "Video Streamer"))
-        self.DataBase.setText(_translate("MyInterface", "Data Base"))
-      
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.VideoStream.setText(_translate("Dialog", "Video Streamer"))
+        self.DataSet.setText(_translate("Dialog", "Data Set"))
+        
+    
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MyInterface = QtWidgets.QDialog()
-    ui = Ui_MyInterface()
-    ui.setupUi(MyInterface)
-    MyInterface.show()
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
     sys.exit(app.exec_())
-
